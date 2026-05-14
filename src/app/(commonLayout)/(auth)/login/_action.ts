517e22d3586a8f4f2ef8bbc78445a856cd7dd1cd
unknown <designwithtawhid@gmail.com>
2026-05-14 15:14:45 +0600

    update the token system

---
+++ src/app/(commonLayout)/(auth)/login/_action.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { httpClient } from "@/lib/axios/httpClient";
+import { setTokenInCookies } from "@/lib/tokenUtils";
+import { ApiErrorResponse } from "@/types/api.types";
+import { ILoginResponse } from "@/types/auth.types";
+import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
+import { redirect } from "next/navigation";
+
+export const LoginAction = async (
+  payload: ILoginPayload,
+): Promise<ILoginResponse | ApiErrorResponse> => {
+  const parsedPayload = loginZodSchema.safeParse(payload);
+  if (!parsedPayload.success) {
+    const firstError = parsedPayload.error.issues[0].message || "Invalid input";
+    return {
+      success: false,
+      message: firstError,
+    };
+  }
+  try {
+    const response = await httpClient.post<ILoginResponse>(
+      "/auth/login",
+      parsedPayload.data,
+    );
+    const { accessToken, refreshToken, token } = response.data;
+    await setTokenInCookies("accessToken", accessToken);
+    await setTokenInCookies("refreshToken", refreshToken);
+    await setTokenInCookies("better-auth.session_token", token);
+    redirect("/dashboard");
+  } catch (error) {
+    return {
+      success: false,
+      message: `login failed : ${error instanceof Error ? error.message : "Unknown error"}`,
+    };
+  }
+};
