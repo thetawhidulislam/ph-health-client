1bdfe5b7b225d6a169eb45a37f7002e457aafe3f
unknown <designwithtawhid@gmail.com>
2026-05-17 18:19:15 +0600

    yRefreshToken check in axios side

--- src/lib/axios/httpClient.ts
+++ src/lib/axios/httpClient.ts
@@ -1,17 +1,48 @@
 import { ApiResponse } from "@/types/api.types";
 import axios from "axios";
+import { isTokenExpiringSoon } from "../tokenUtils";
+import { cookies, headers } from "next/headers";
+import { getNewTokenWithRefreshToken } from "@/services/auth.service";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 if (!API_BASE_URL) {
   throw new Error("API_BASE_URL is not defined in environment variables");
 }
+async function tryRefreshToken(
+  accessToken: string,
+  refreshToken: string,
+): Promise<void> {
+  if (!isTokenExpiringSoon(accessToken)) {
+    return;
+  }
+  const requestHeader = await headers();
+  if (requestHeader.get("x-token-refreshed") === "1") {
+    return;
+  }
+  try {
+    await getNewTokenWithRefreshToken(refreshToken);
+  } catch (error) {
+    console.error("Error refreshing token:", error);
+  }
+}
 
-const axiosInstance = () => {
+const axiosInstance = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+  if (accessToken && refreshToken) {
+    await tryRefreshToken(accessToken, refreshToken);
+  }
+  const cookieHeader = cookieStore
+    .getAll()
+    .map((cookie) => `${cookie.name}=${cookie.value}`)
+    .join("; ");
   const instance = axios.create({
     baseURL: API_BASE_URL,
     timeout: 30000,
     headers: {
       "Content-Type": "application/json",
+      Cookie: cookieHeader,
     },
   });
   return instance;
@@ -25,7 +56,7 @@ const httpGet = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const instance = axiosInstance();
+    const instance = await axiosInstance();
     const response = await instance.get<ApiResponse<TData>>(endpoint, {
       params: options?.params,
       headers: options?.headers,
@@ -43,14 +74,11 @@ const httpPost = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().post<ApiResponse<TData>>(
-      endpoint,
-      data,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+    const response = await instance.post<ApiResponse<TData>>(endpoint, data, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error("HTTP POST Error:", error);
@@ -63,14 +91,12 @@ const httpPut = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().put<ApiResponse<TData>>(
-      endpoint,
-      data,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+
+    const response = await instance.put<ApiResponse<TData>>(endpoint, data, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error("HTTP PUT Error:", error);
@@ -83,14 +109,12 @@ const httpPatch = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().patch<ApiResponse<TData>>(
-      endpoint,
-      data,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+
+    const response = await instance.patch<ApiResponse<TData>>(endpoint, data, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error("HTTP PATCH Error:", error);
@@ -102,13 +126,12 @@ const httpDelete = async <TData>(
   options?: ApiRequestOptions,
 ): Promise<ApiResponse<TData>> => {
   try {
-    const response = await axiosInstance().delete<ApiResponse<TData>>(
-      endpoint,
-      {
-        params: options?.params,
-        headers: options?.headers,
-      },
-    );
+    const instance = await axiosInstance();
+
+    const response = await instance.delete<ApiResponse<TData>>(endpoint, {
+      params: options?.params,
+      headers: options?.headers,
+    });
     return response.data;
   } catch (error) {
     console.error("HTTP DELETE Error:", error);
