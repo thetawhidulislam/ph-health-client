563e67e9a667f8f16a245abc117972a0998584fa
unknown <designwithtawhid@gmail.com>
2026-05-18 21:52:22 +0600

    updated nav items and dashboard

--- src/components/modules/Dashboard/DashboardSidebar.tsx
+++ src/components/modules/Dashboard/DashboardSidebar.tsx
@@ -1,7 +1,16 @@
+import { getDefaultDashboardRoute } from "@/lib/authUtils";
+import { getNavItemsByRole } from "@/lib/navItems";
+import { getUserInfo } from "@/services/auth.service";
+import { NavSection } from "@/types/dashboard.type";
 import React from "react";
 
-const DashboardSidebar = () => {
-  return <div>DashboardSidebar</div>;
+const DashboardSidebar = async () => {
+  const userInfo = await getUserInfo();
+  const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
+
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role);
+
+  return <div>DashboardSidebars  </div>;
 };
 
 export default DashboardSidebar;
