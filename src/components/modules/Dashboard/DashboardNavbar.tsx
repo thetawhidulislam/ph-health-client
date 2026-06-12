6eead1cb35c7044416d6f1232320dbd06636baf7
unknown <designwithtawhid@gmail.com>
2026-06-12 18:06:55 +0600

    updated navbr

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -3,13 +3,20 @@ import { getNavItemsByRole } from "@/lib/navItems";
 import { getUserInfo } from "@/services/auth.service";
 import { NavSection } from "@/types/dashboard.type";
 import React from "react";
+import DashboardNavbarContent from "./DashboardNavbarContent";
 
 const DashboardNavbar = async () => {
   const userInfo = await getUserInfo();
   const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
 
   const dashboardHome = getDefaultDashboardRoute(userInfo.role);
-  return <div>DashboardNavbar</div>;
+  return (
+    <DashboardNavbarContent
+      userInfo={userInfo}
+      navItems={navItems}
+      dashboardHome={dashboardHome}
+    ></DashboardNavbarContent>
+  );
 };
 
 export default DashboardNavbar;
--- src/components/modules/Dashboard/DashboardNavbarContent.tsx
+++ src/components/modules/Dashboard/DashboardNavbarContent.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { NavSection } from "@/types/dashboard.type";
 import { UserInfo } from "@/types/user.types";
-
 import React, { useState } from "react";
+import DashboardMobileSidebar from "./DashboardMobileSidebar";
+import { MenuIcon, Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import NotificationDropdown from "./NotificationDropdown";
 
 interface DashboardNavbarContentProps {
   userInfo: UserInfo;
@@ -19,12 +24,33 @@ const DashboardNavbarContent = ({
   return (
     <>
       {/* Mobile menu toggle icon */}
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger className="md:hidden">
+          <Button variant={"outline"} size="icon">
+            <MenuIcon className="w-4 h-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="p-0 w-64">
+          <DashboardMobileSidebar
+            userInfo={userInfo}
+            navItems={navItems}
+            dashboardHome={dashboardHome}
+          />
+        </SheetContent>
+      </Sheet>
 
       {/* search component */}
+      <div className="flex-1 flex items-center">
+        <div className="relative w-full hidden sm:block">
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+          <Input type="text" placeholder="Search..." className="pl-9 pr-4" />
+        </div>
+      </div>
 
       {/* Right side Actions */}
 
       {/* notification */}
+      <NotificationDropdown />
 
       {/* user dropdown */}
     </>
