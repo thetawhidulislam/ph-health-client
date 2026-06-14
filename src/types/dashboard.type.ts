1893a4f54beab2bde4a8cd826e8dc3fa3708e9a2
unknown <designwithtawhid@gmail.com>
2026-06-14 23:11:10 +0600

    added pie chart and barchart

--- src/types/dashboard.type.ts
+++ src/types/dashboard.type.ts
@@ -8,3 +8,12 @@ export interface NavSection {
   title?: string;
   items: NavItem[];
 }
+export interface PieChartData {
+  status: string;
+  count: number;
+}
+
+export interface BarChartData {
+  month: Date | string;
+  count: number;
+}
